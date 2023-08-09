10e211d8bc1107ad4c65027e5bf9988d5692f8d0
Khushi Sharma <92661660+kkhushiie@users.noreply.github.com>
2023-08-09 19:25:38 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,21 +23,21 @@ export default function Header() {
      <Box display="flex" justifyContent="flex-end" sx={{ height: '30vh', marginTop:"-270px"}}>
      <Stack style={{padding:"70px"}} spacing={1}>
      <Typography variant="h4" color={'black'} lineHeight={'40px'} fontWeight={'900'}  gutterBottom>
-      FLAC
+      ABC
       </Typography>
       <Typography variant="h4" color={'black'} lineHeight={'20px'} fontWeight={'900'}   gutterBottom>
-      FOOD
+      XYZ
       </Typography>
-      <Typography variant="h6" color={'black'} gutterBottom>Home cooked meals at your doorstep</Typography>
+      <Typography variant="h6" color={'black'} gutterBottom>Lorem ipsum dolor sit.</Typography>
       <Button className='btn' style={{ backgroundColor: "#da753a" }} variant="contained">Visit</Button>
     </Stack> 
     </Box>
    </Container>
      <Stack mt={"00px"} padding={{xs:"0px", sm:"70px"}} paddingRight={{sm:"200px"}} direction={{ xs: 'row-reverse', sm: 'row-reverse' }}
      spacing={{ xs: 1, sm: 2, md: 8 }} >
-     <Button style={{ marginTop: '50px', color:"black" ,marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#E4E4E4", borderRadius: "20px",  width:"120px",  height:"140px", backgroundImage:`url(/FLAC/living.png)`,backgroundPositionX:"center" ,backgroundSize: "90px 90px"}} variant="contained"><Typography className='button-text'>Flac Community</Typography></Button>
-     <Button style={{ marginTop: '50px', color:"black",  marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#E4E4E4", borderRadius: "20px",  width:"120px",  height:"140px", backgroundImage:`url(/FLAC/seat.png)`, backgroundPositionX: "center", backgroundSize: "110px 110px"}} variant="contained"><Typography className='button-text'>Flac Living</Typography></Button>
-     <Button style={{marginTop: '50px', color:"#FE7122",marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#ECECEC", borderRadius: "20px",  width:"120px", height:"140px", backgroundImage:`url(/FLAC/food.png)`, backgroundPosition: "center", backgroundSize: "80px 70px"}} variant="contained"><Typography className='button-text'>Flac Food</Typography></Button>
+     <Button style={{ marginTop: '50px', color:"black" ,marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#E4E4E4", borderRadius: "20px",  width:"120px",  height:"140px", backgroundImage:`url(/FLAC/living.png)`,backgroundPositionX:"center" ,backgroundSize: "90px 90px"}} variant="contained"><Typography className='button-text'>Feature 3</Typography></Button>
+     <Button style={{ marginTop: '50px', color:"black",  marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#E4E4E4", borderRadius: "20px",  width:"120px",  height:"140px", backgroundImage:`url(/FLAC/seat.png)`, backgroundPositionX: "center", backgroundSize: "110px 110px"}} variant="contained"><Typography className='button-text'>Feature 2</Typography></Button>
+     <Button style={{marginTop: '50px', color:"#FE7122",marginBottom:"4px", fontWeight: "bold", backgroundRepeat:"no-repeat", backgroundColor: "#ECECEC", borderRadius: "20px",  width:"120px", height:"140px", backgroundImage:`url(/FLAC/food.png)`, backgroundPosition: "center", backgroundSize: "80px 70px"}} variant="contained"><Typography className='button-text'>Feature 1</Typography></Button>
      </Stack>
      </div>
     );
