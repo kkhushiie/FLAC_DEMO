c4aa551a2b4833467ae8e31f58f5f3aa5f779af5
Khushi Sharma <92661660+kkhushiie@users.noreply.github.com>
2023-08-09 19:23:55 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default function About() {
           <Typography variant="body2" color={'white'} fontFamily="Poppins" font-style='normal' fontWeight={400} fontSize={'28px'}
             lineHeight={"42px"}
             letterSpacing={'0.01em'} align='justify' paddingBottom={'85px'} marginBottom={'0px'} gutterBottom>
-            We love home-cooked meals and we want you to love them too. Our platform connects you with housewives who cook tasty and healthy meals for you. You can pick from different dishes and get them delivered to your door. Our platform helps both you and the housewives. You enjoy delicious and nutritious food that feels like home. The housewives share their passion for cooking and earn money from their kitchen. We care about the quality of our food and the environment. We use fresh and local ingredients in our meals. We support local communities and eco-friendly practices. We make our platform easy and convenient for you. We value your feedback and suggestions and improve our service.
+           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros.Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum erosCurabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros.Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros.
           </Typography>
         </Container>
       </div>
